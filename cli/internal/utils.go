3f91b60dd2ceec7f1622b528383a3ddc3f847d4b
octav1an <octav1an@hotmail.com>
2024-11-17 15:58:26 +0100

    move utils function to internal package

---
+++ cli/internal/utils.go
@@ -0,0 +1,19 @@
+package internal
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func BToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
+
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v MiB", BToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", BToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", BToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
--- cli/main.go
+++ cli/main.go
@@ -137,19 +137,6 @@ func (fp *FileProcessor) deepCompare(filePath string, compareToFilePath string) 
 	return isDup, nil
 }
 
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
-}
-
-func printMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-}
-
 func main() {
 	internal.PrintMemUsage()
 	argsParser := internal.NewArgsParser()
